83a9d70de5b5ead1da2254187d76daa1e21c3f20
Aung Htet <aung9htet.ah@gmail.com>
2022-05-09 05:48:02 +0100

    Fixed and calibrated exploration

--- robot_explore.py
+++ robot_explore.py
@@ -29,43 +29,46 @@ class RobotExplore:
         
         # set variables for project
         self.speed = 0.4
-        self.min_wall = 0.2
+        self.min_wall = 0.5
         self.turn_angle = 0
         self.start_time = rospy.get_rostime()
-        self.start_turn = 0
         self.explore_robot = True
 
     # check whether to turn
     def turn_robot(self):
+        time_elasped = rospy.get_rostime().secs - self.start_time.secs
         # check if robot is in the middle of turning
         if self.explore_robot == False:
-            if ((rospy.get_rostime().secs - self.start_turn.secs) % 60) == 0:
+            if ((time_elasped % 5) == 0):
                 self.explore_robot = True
-            print("holy crap")
-            return True
-        # check if the wall is too near
-        if self.wall_distance < self.min_wall:
-            self.start_turn = rospy.get_rostime()
-            self.explore_robot = False
-            print("lol")
+            print("turning", self.robot_range.range)
             return True
         else:
-            return False
+            # check if the wall is too near
+            if self.robot_range.range < self.min_wall:
+                self.start_turn = rospy.get_rostime()
+                self.explore_robot = False
+                print("wall ahead", self.robot_range.range)
+                return True
+            else:
+                print("going straight")
+                return False
     
     # to explore randomly around
     def explore(self):
         # get time elasped
         time_elasped = rospy.get_rostime().secs - self.start_time.secs
-        self.wall_distance = self.robot_range.range
         # set maximum angle of turns to be 10 degrees
         if self.turn_robot() == False:
             if ((time_elasped % 2) == 0):
-                max_angle_turn = ((2 * np.pi)/360) * 80
+                print("change angle on straight")
+                max_angle_turn = np.pi/2 # 90 degrees max angle
                 rand_turn = np.random.randint(-np.ceil(max_angle_turn * 10), np.ceil(max_angle_turn * 10))/10
                 self.turn_angle = rand_turn
             self.robot_movement.set_move_cmd(linear=self.speed, angular=self.turn_angle)
             self.robot_movement.vel_publish()
         else:
+            print("rotate")
             angle_turn = (np.pi/2)/3 * 2
             self.robot_movement.set_move_cmd(linear=0, angular=angle_turn)
             self.robot_movement.vel_publish()
