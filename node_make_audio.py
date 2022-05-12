f4003341d62474c11983f4be103be6ba14fe809d
akhilmakes <58556461+akhilmakes@users.noreply.github.com>
2022-05-12 15:42:07 +0100

    Sound Working

--- node_make_audio.py
+++ node_make_audio.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import os
 import rospy    # ROS Python interface
-from std_msgs.msg import Int16MultiArray 
+from std_msgs.msg import UInt16MultiArray 
 
 import miro2 as miro
 
@@ -11,14 +11,12 @@ class NodeMakeAudio(object):
 
         topic_base_name = "/" + os.getenv("MIRO_ROBOT_NAME")
         self.sound_pub = rospy.Publisher(
-            topic_base_name + "/sensors/mics", Int16MultiArray, queue_size=0
+            topic_base_name + "/control/tone", UInt16MultiArray, queue_size=0
         )
         self.sound_detail = [1000,50,250]
 
-    def set_sound_cmd(self, freq = 0, volume = 0, duration = 0):
-        self.mic_generate = Int16MultiArray()
+    def produce_sound(self, freq = 0, volume = 0, duration = 0):
+        self.mic_generate = UInt16MultiArray()
         self.sound_detail = [freq, volume, duration]
         self.mic_generate.data = self.sound_detail
-
-    def pub_sound(self):
         self.sound_pub.publish(self.mic_generate)
