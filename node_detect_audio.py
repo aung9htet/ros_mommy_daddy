813eb17f2b0a3a79aa00dce02884d748deb81954
Glen <73548491+glenbpalm@users.noreply.github.com>
2022-05-12 02:40:43 +0100

    Changed detect_audio file

--- node_detect_audio.py
+++ node_detect_audio.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+##!/usr/bin/env python
 import os
 import rospy
 from std_msgs.msg import Int16MultiArray
@@ -10,15 +10,15 @@ class NodeDetectAudio(object):
 	def __init__(self):
 
 		topic_base_name = "/" + os.getenv("MIRO_ROBOT_NAME")
-		self.sound_sub = rospy.Subscriber(
+		self.detect_sound_pub = rospy.Publisher(
 			topic_base_name + "/control/tone", Int16MultiArray, queue_size=0
 		)
-		self.sound_detected = False
-		self.care_sound = [1000,50,250]
+		self.care_sound = [0,0,0]
 
-		def detect_sound_cmd(self, freq = 0, volume = 0, duration = 0):
-			self.mic_receive = Int16MultiArray()
-			self.sound_detail = [freq, volume, duration]
-			
-			if self.sound_detail == self.care_sound:
-				self.sound_detected = True
+	def detect_sound_cmd(self, freq = 0, volume = 0, duration = 0):
+		self.mic_receive = Int16MultiArray()
+		self.sound_detail = [freq, volume, duration]
+		self.mic_receive.data = self.sound_detail
+
+	def pub_detect_sound(self):
+		self.detect_sound_pub.publish(self.mic_receive)
